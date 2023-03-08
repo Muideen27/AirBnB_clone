8744fc0ceb42b49db8e09b6fd424f15cb1a9ab7d
Torbary <torbarey020@gmail.com>
2023-03-08 07:13:45 -0800

    updated my cmd interpreter[show]

--- console.py
+++ console.py
@@ -2,11 +2,45 @@
 """Module for HBNB command interpreter"""
 
 import cmd
+import re
+import models
+from shlex import split
+from models import storage
+
+
+def parse(arg):
+    """Look for curly braces and square_brackets match using re"""
+    curly_brace_match = re.search(r"\{(.*?)\}", arg)
+    square_brackets_match = re.search(r"\[(.*?)\]", arg)
+
+    if curly_brace_match is None:
+        if square_brackets_match is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            tokens = arg[:square_brackets_match.span()[0]].split()
+            token = [token.strip(",") for token in tokens]
+            token.append(square_brackets_match.group())
+            return token
+    else:
+        tokens = arg[:curly_brace_match.span()[0]].split()
+        token = [token.strip(",") for token in tokens]
+        token.append(curly_brace_match.group())
+        return token
 
 class HBNBCommand(cmd.Cmd):
     """class for HBNB command interpreter"""
 
     prompt = "(hbnb) "
+
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     def do_create(self, arg):
 
@@ -21,6 +55,24 @@ class HBNBCommand(cmd.Cmd):
             print(obj.id)
         except NameError:
             print("** class doesn't exist **")
+
+    
+    def do_show(self, arg):
+        """Usage: show <class> <id> or <class>.show(<id>)
+        Display the string representation of a class instance of a given id.
+        """
+        argl = parse(arg)
+        objdict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict:
+            print("** no instance found **")
+        else:
+            print(objdict["{}.{}".format(argl[0], argl[1])])
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
